a937696c515f4d38bdb6e5e8c74c0fd4c8ff5761
Stiven <89946683+thisfeeling@users.noreply.github.com>
2023-11-13 19:38:20 -0500

    Update anykernel.sh

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Proximity kernel by PrabhatProxy
+kernel.string=Ali kernel by thisfeeling_dev
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
