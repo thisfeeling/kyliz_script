80106a1e32e304a2f729b57142e48c5d0ff7fa02
Stiven <89946683+thisfeeling@users.noreply.github.com>
2023-11-22 21:10:16 -0500

    Update anykernel.sh
    kernel.string=Moto G(6) Custom Kernel by thisfeeling

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ali kernel by thisfeeling_dev
+kernel.string=Moto G(6) Custom Kernel by thisfeeling
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
